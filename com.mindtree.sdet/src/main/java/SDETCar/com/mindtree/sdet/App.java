0a732d96b8d773054307cf83b2ab7fbeccce90d5
M1046811 <Pulkit.Sukhija2@mindtree.com>
2018-09-11 01:33:02 +0530

    this is new

---
+++ com.mindtree.sdet/src/main/java/SDETCar/com/mindtree/sdet/App.java
@@ -0,0 +1,13 @@
+package SDETCar.com.mindtree.sdet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
